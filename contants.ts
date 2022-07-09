e16f8544281c8ec8d5a77fc4fe1a29b363353213
igorovic <758832+igorovic@users.noreply.github.com>
2022-07-09 18:14:34 +0200

    fix: redix prefix

--- contants.ts
+++ contants.ts
@@ -1,2 +1,4 @@
 export const repositoryUrl = "https://github.com/igorovic/portfolio-app";
-export const redisPrefix = "portfolio:";
+export const redisPrefix = `portfolio:${
+  process.env.VERCEL ? String(process.env.VERCEL_ENV).slice(0, 3) : "local"
+}:`;
