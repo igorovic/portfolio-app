4d1d1b84e9460b3e34be434a6039067952b258f8
igorovic <hello@dyve.dev>
2022-07-14 17:00:09 +0200

    wip

--- components/canva/canva.tsx
+++ components/canva/canva.tsx
@@ -1,23 +1,29 @@
 import { Button } from "@mantine/core";
-import React, { useEffect } from "react";
+import React from "react";
 import ToolBox from "./ToolBox";
 import { store } from "./store";
 import { Provider } from "react-redux";
-import { render } from "./render";
+import { handleClick, render } from "./render";
+import { canvasId } from "./constants";
 
 function Canva() {
-  const canvasId = "canva";
+  const canvaRef = React.useRef<HTMLCanvasElement>(null);
   const ctxRef = React.useRef<CanvasRenderingContext2D | null>(null);
-  //const timestampRef = React.useRef<number>();
 
-  useEffect(() => {
+  React.useEffect(() => {
     ctxRef.current = (
       document.getElementById(canvasId) as HTMLCanvasElement
     )?.getContext("2d");
     const unsubscribe = store.subscribe(() => {
-      render(canvasId, store.getState().shape);
+      render();
     });
     return unsubscribe;
+  });
+
+  React.useEffect(() => {
+    const canva = canvaRef.current;
+    canva?.addEventListener("click", handleClick);
+    return () => canva?.removeEventListener("click", handleClick);
   });
 
   return (
@@ -25,32 +31,43 @@ function Canva() {
       <div className="relative">
         <ToolBox canvasId={canvasId} />
         <canvas
+          ref={canvaRef}
           id={canvasId}
           width={600}
           height={600}
           className="border border-blue-100 w-[600px] h-[600px]"
-        ></canvas>
-        <Button
-          onClick={() => {
-            const ctx = ctxRef.current;
-
-            ctx?.clearRect(
-              0,
-              0,
-              ctx.canvas.clientWidth,
-              ctx.canvas.clientHeight
-            );
-          }}
         >
-          reset
-        </Button>
-        <Button
-          onClick={() => {
-            render(canvasId, store.getState().shape);
-          }}
-        >
-          render
-        </Button>
+          <button id="button1">Continue</button>
+        </canvas>
+        <div className="flex flex-wrap gap-2">
+          <Button
+            onClick={() => {
+              const ctx = ctxRef.current;
+              ctx?.clearRect(
+                0,
+                0,
+                ctx.canvas.clientWidth,
+                ctx.canvas.clientHeight
+              );
+            }}
+          >
+            reset
+          </Button>
+          <Button
+            onClick={() => {
+              ctxRef.current?.save();
+            }}
+          >
+            save
+          </Button>
+          <Button
+            onClick={() => {
+              ctxRef.current?.restore();
+            }}
+          >
+            restore
+          </Button>
+        </div>
       </div>
     </Provider>
   );
--- components/canva/render.ts
+++ components/canva/render.ts
@@ -1,15 +1,34 @@
-export const render = (canvasId: string, o: any) => {
+import { canvasId } from "./constants";
+import { store } from "./store";
+let clickPosition: { x: number; y: number } | null = null;
+
+export function handleClick(e: MouseEvent) {
+  const ctx = (
+    document.getElementById("canva") as HTMLCanvasElement
+  )?.getContext("2d");
+  clickPosition = { x: e.offsetX, y: e.offsetY };
+  console.debug(e);
+  render();
+}
+
+export const render = () => {
+  const o = store.getState().shape;
   if (typeof window === "undefined") return;
+  const canva = document.getElementById(canvasId) as HTMLCanvasElement | null;
 
   window.requestAnimationFrame(() => {
-    const canva = document.getElementById(canvasId) as HTMLCanvasElement | null;
     const ctx = canva?.getContext("2d");
     if (!ctx) {
       return console.error("canvas context is undefined");
     }
 
     ctx?.clearRect(0, 0, ctx.canvas.clientWidth, ctx.canvas.clientHeight);
+    console.debug("click position", clickPosition);
     drawRect(ctx, o);
+
+    if (clickPosition && ctx.isPointInPath(clickPosition.x, clickPosition.y)) {
+      console.debug("click on shape detected");
+    }
   });
 };
 
@@ -17,40 +36,51 @@ function drawRect(ctx: CanvasRenderingContext2D, o: any) {
   if (o.fill) {
     ctx.fillStyle = o.fill;
   }
-  if (o.rtl) {
-    ctx.beginPath();
-    ctx.moveTo(topLeft(o).x + o.rtl, topLeft(o).y);
-    ctx.lineTo(topRight(o).x - o.rtr, topRight(o).y);
-    ctx.quadraticCurveTo(
-      ...topRight(o).xy,
-      topRight(o).x,
-      topRight(o).y + o.rtr
-    );
-    ctx.lineTo(bottomRight(o).x, bottomRight(o).y - o.rbr);
-    ctx.quadraticCurveTo(
-      ...bottomRight(o).xy,
-      bottomRight(o).x - o.rbr,
-      bottomRight(o).y
-    );
-    ctx.lineTo(bottomLeft(o).x + o.rbl, bottomLeft(o).y);
-    ctx.quadraticCurveTo(
-      ...bottomLeft(o).xy,
-      bottomLeft(o).x,
-      bottomLeft(o).y - o.rbl
-    );
-    ctx.lineTo(topLeft(o).x, topLeft(o).y + o.rtl);
-    ctx.quadraticCurveTo(...topLeft(o).xy, topLeft(o).x + o.rtl, topLeft(o).y);
-    ctx.closePath();
-    if (o.fill) {
-      ctx.fill();
-    }
+  const P = svgRectPath(o);
+  //   ctx.beginPath();
+  //   ctx.moveTo(topLeft(o).x + o.rtl, topLeft(o).y);
+  //   ctx.lineTo(topRight(o).x - o.rtr, topRight(o).y);
+  //   ctx.quadraticCurveTo(...topRight(o).xy, topRight(o).x, topRight(o).y + o.rtr);
+  //   ctx.lineTo(bottomRight(o).x, bottomRight(o).y - o.rbr);
+  //   ctx.quadraticCurveTo(
+  //     ...bottomRight(o).xy,
+  //     bottomRight(o).x - o.rbr,
+  //     bottomRight(o).y
+  //   );
+  //   ctx.lineTo(bottomLeft(o).x + o.rbl, bottomLeft(o).y);
+  //   ctx.quadraticCurveTo(
+  //     ...bottomLeft(o).xy,
+  //     bottomLeft(o).x,
+  //     bottomLeft(o).y - o.rbl
+  //   );
+  //   ctx.lineTo(topLeft(o).x, topLeft(o).y + o.rtl);
+  //   ctx.quadraticCurveTo(...topLeft(o).xy, topLeft(o).x + o.rtl, topLeft(o).y);
+  //   ctx.closePath();
+  if (o.fill) {
+    ctx.fill(P);
   } else {
-    if (o.fill) {
-      ctx.fillRect(o.x, o.y, o.w, o.h);
-    } else {
-      ctx.rect(o.x, o.y, o.w, o.h);
-    }
+    ctx.stroke(P);
   }
+}
+
+function svgRectPath(o: any): Path2D {
+  const tr = topRight(o);
+  const tl = topLeft(o);
+  const bl = bottomLeft(o);
+  const br = bottomRight(o);
+  const svgPath = `
+  M ${tl.x + o.rtl} ${tl.y} 
+  H ${tr.x - o.rtr} 
+  Q ${tr.x} ${tr.y} ${tr.x} ${tr.y + o.rtr} 
+  V ${br.y - o.rbr} 
+  Q ${br.x} ${br.y} ${br.x - o.rbr} ${br.y}
+  H ${bl.x + o.rbl} 
+  Q ${bl.x} ${bl.y} ${bl.x} ${bl.y - o.rbl}
+  V ${tl.y + o.rtl}
+  Q ${tl.x} ${tl.y} ${tl.x + o.rtl} ${tl.y}
+  `;
+  console.debug(svgPath);
+  return new Path2D(svgPath);
 }
 
 function topLeft(obj: { x: number; y: number }) {
