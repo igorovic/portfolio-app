cc9d8b17dc565575d7da03f0dab55141a93aed46
igorovic <hello@dyve.dev>
2022-07-21 08:00:59 +0200

    feat: add translations

--- pages/app/twitter-download.tsx
+++ pages/app/twitter-download.tsx
@@ -30,6 +30,7 @@ export async function getStaticProps({ locale }: GetStaticPropsContext) {
         "footer",
         "appshell",
         "validation-errors",
+        "twitter-download/tdl",
       ])),
       // Will be passed to the page component as props
     },
