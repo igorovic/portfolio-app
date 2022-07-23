7178a3d915f92757098fca7ace7d83cb7ce1aec4
igorovic <758832+igorovic@users.noreply.github.com>
2022-07-23 17:33:02 +0200

    wip

--- features/pdf-embed/PdfEmbed.tsx
+++ features/pdf-embed/PdfEmbed.tsx
@@ -1,60 +1,95 @@
 import DropZone from "components/dropzone/DropZone";
 import { useAtom } from "jotai";
-import Script from "next/script";
-import { useCallback, useEffect } from "react";
+import { useRouter } from "next/router";
+
+import { useCallback, useEffect, useRef, useState } from "react";
 import { pdfUrlObject } from "./store";
+import * as pdfJs from "pdfjs-dist";
+import { Button, Slider } from "@mantine/core";
+import Two from "two.js";
+pdfJs.GlobalWorkerOptions.workerSrc = "/pdf.worker.min.js";
 
 function PdfEmbed() {
+  const canvasId = "pdfjs-canvas";
+  const { locale } = useRouter();
   const [pdfUrlObj, setPdfUrlObj] = useAtom(pdfUrlObject);
-  let adobeDCView: any;
+  console.debug(pdfUrlObj);
+  const pageRef = useRef<pdfJs.PDFPageProxy | null>(null);
+  const [zoom, setZoom] = useState(1);
+  const twoRef = useRef<Two | null>(null);
+
+  const render = useCallback(() => {
+    // Display page on the existing canvas with 100% scale.
+    if (!pageRef.current) return;
+    const page = pageRef.current;
+    const canvas = document.getElementById(canvasId) as HTMLCanvasElement;
+    const viewport = page.getViewport({ scale: zoom });
+    canvas.width = viewport.width;
+    canvas.height = viewport.height;
+    const ctx = canvas.getContext("2d") as CanvasRenderingContext2D;
+    const renderTask = page.render({
+      canvasContext: ctx,
+      viewport,
+    });
+    return renderTask.promise;
+  }, [zoom]);
+
   const onDrop = useCallback(
     (files: FileList) => {
-      console.debug(files);
-      if (!adobeDCView || files.length === 0) return;
+      if (files.length === 0) return;
       const file = files[0];
       const fileObjUrl = URL.createObjectURL(new Blob([file]));
       setPdfUrlObj(fileObjUrl);
-      const reader = new FileReader();
-      reader.onloadend = function (e) {
-        if (!e.target) return;
-
-        var filePromise = Promise.resolve(e.target?.result);
-        adobeDCView.previewFile(
-          {
-            content: {
-              promise: filePromise,
-            },
-            metaData: { fileName: file.name },
-          },
-          {
-            embedMode: "SIZED_CONTAINER",
-          }
-        );
-      };
-      reader.readAsArrayBuffer(file);
+      const loadingTask = pdfJs.getDocument(fileObjUrl);
+      loadingTask.promise.then((pdf) => {
+        // Request a first page
+        return pdf.getPage(1).then(function (pdfPage) {
+          pageRef.current = pdfPage;
+          render();
+        });
+      });
     },
-    [adobeDCView, setPdfUrlObj]
+    [render, setPdfUrlObj]
   );
 
-  const adobeDCViewReady = () => {
-    //@ts-expect-error
-    adobeDCView = new AdobeDC.View({
-      clientId: `${process.env.NEXT_PUBLIC_ADOBE_APIKEY_PDFEMBED}`,
-      divId: "adobe-dc-view",
-    });
-  };
-
   useEffect(() => {
-    document.addEventListener("adobe_dc_view_sdk.ready", adobeDCViewReady);
-    return () =>
-      document.removeEventListener("adobe_dc_view_sdk.ready", adobeDCViewReady);
+    if (!twoRef.current) {
+      twoRef.current = new Two({
+        type: "canvas",
+        domElement: document.getElementById(canvasId) as HTMLCanvasElement,
+      });
+    }
   });
+
+  const drawCircle = useCallback(() => {
+    if (!twoRef.current) return;
+    const two = twoRef.current;
+    const C = two.makeCircle(20, 20, 32);
+    C.fill = "rgb(255, 100, 100)";
+    C.noStroke();
+    two.render();
+  }, []);
 
   return (
     <>
-      <Script src="https://documentcloud.adobe.com/view-sdk/main.js" />
       <DropZone onDrop={onDrop} label="drag and drop pdf here" />
-      <div id="adobe-dc-view" className="mt-4 h-full"></div>
+      <div className="pt-4 flex justify-around">
+        <Button onClick={() => render()}>re-render</Button>
+        <Button onClick={drawCircle}>draw circle</Button>
+      </div>
+      <div>
+        <Slider
+          defaultValue={1}
+          min={0.1}
+          max={4}
+          label={(value) => value.toFixed(1)}
+          step={0.1}
+          styles={{ markLabel: { display: "none" } }}
+          onChange={setZoom}
+          onChangeEnd={() => render()}
+        />
+      </div>
+      <canvas id={canvasId} width={600} height={600}></canvas>
     </>
   );
 }
